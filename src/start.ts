b9d93173cd3b433c1ab3f81cc362e45cbe846f3b
沈赟杰 <shenyunjie@znrank.com>
2024-04-04 15:12:59 +0800

    update

--- src/start.ts
+++ src/start.ts
@@ -16,28 +16,7 @@ export async function BlogStart(file: string = 'package.json') {
   }
 
   const mainModulePath = require.resolve('@pjblog/blog');
-  const { default: Blog } = await import(mainModulePath);
+  const { default: Blog, findPlugins } = await import(mainModulePath);
   const dependencies = findPlugins(Object.keys(pkg.dependencies));
   await Blog(pkg.blog, dependencies);
-}
-
-async function findPlugins(dependencies: string[]) {
-  const plugin: any[] = [];
-  for (let i = 0; i < dependencies.length; i++) {
-    const dependency = dependencies[i];
-    if (matchTheme(dependency) || matchPlugin(dependency)) {
-      const path = require.resolve(dependency);
-      const { default: Plugin } = await import(path);
-      plugin.push(Plugin);
-    }
-  }
-  return plugin;
-}
-
-function matchTheme(name: string) {
-  return name.startsWith('pjblog-theme-');
-}
-
-function matchPlugin(name: string) {
-  return name.startsWith('pjblog-plugin-');
 }
